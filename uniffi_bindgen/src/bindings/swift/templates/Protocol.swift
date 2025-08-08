55c520bf0eb8ba4a12591183ce67f3e347469ca9
DuyLe <duylh2@vng.com.vn>
2025-08-08 11:33:45 +0700

    update

--- uniffi_bindgen/src/bindings/swift/templates/Protocol.swift
+++ uniffi_bindgen/src/bindings/swift/templates/Protocol.swift
@@ -2,6 +2,7 @@
 public protocol {{ protocol_name }}: AnyObject, Sendable {
     {% for meth in methods.iter() -%}
     {%- call swift::docstring(meth, 4) %}
+    {% call swift::availability_async(meth) -%}
     func {{ meth.name()|fn_name }}({% call swift::arg_list_protocol(meth) %}) {% call swift::is_async(meth) -%}{% call swift::throws(meth) -%}
     {%- match meth.return_type() -%}
     {%- when Some with (return_type) %} -> {{ return_type|type_name -}}
